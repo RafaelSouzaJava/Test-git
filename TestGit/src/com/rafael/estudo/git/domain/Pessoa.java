b058ab36b2d86384dfb1dd7869ee2ad3791954eb
rafae <rafae@DESKTOP-BIIAEIQ>
2023-02-28 19:11:59 -0300

    teste de reversão

--- TestGit/src/com/rafael/estudo/git/domain/Pessoa.java
+++ TestGit/src/com/rafael/estudo/git/domain/Pessoa.java
@@ -1,5 +1,32 @@
 package com.rafael.estudo.git.domain;
 
+import java.util.Date;
+
 public class Pessoa {
+	
+	private String nome;
+	private String idade;
+	private Date dataNascimento;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getIdade() {
+		return idade;
+	}
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
 
 }
